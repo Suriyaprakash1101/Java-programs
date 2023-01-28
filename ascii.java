2d7280e1488d4622dd5eb361474c1d68623ba380
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-01-28 08:35:39 +0530

    Find acii value for characters

---
+++ ascii.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        String n="Suriya";
+        int a;
+        for(int i=0;i<n.length();i++){
+            a=n.charAt(i);
+            System.out.println(a);
+        }
+    }
+}
