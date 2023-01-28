84a44d08e3d7ae412e2b15744bf622373ebd24c5
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-01-28 08:40:59 +0530

    Multiple of two nums

---
+++ multiple.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int a,b;
+        a=10;
+        b=15;
+        int c=a*b;
+        System.out.println(c);
+    }
+}
