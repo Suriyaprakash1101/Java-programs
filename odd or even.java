68b3d2649ea00892498f3870a40102cb7913698b
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-01-28 16:20:36 +0530

    odd or even number

---
+++ odd or even.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class odd or even{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=in.nextInt();
+        if (n%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+}
