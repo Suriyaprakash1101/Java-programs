471db454594762d1c40dd0543d6484103bb22709
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-01-28 09:24:45 +0530

    addition of two nums

---
+++ addition of two numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int a=10;
+        int b=12;
+        int c=a+b;
+        System.out.println("The addition of two numbers is "+c);
+    }
+}
