9f1224cde86fffa01af56385373a87e3e7c777b8
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-02-05 11:24:11 +0530

    swaping method 2

---
+++ swap2.java
@@ -0,0 +1,11 @@
+public class swap2{
+    public static void main(String[] args){
+        int a=4;
+        int b=5;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swaping the value of a is "+a);
+        System.out.println("After swaping the value of b is "+b);
+    }
+}
