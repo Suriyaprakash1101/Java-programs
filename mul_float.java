d961b8f6e9fdbf9d8b59c5d4b8c97566c35782cc
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-02-05 11:03:21 +0530

    multiplication of two float numbers

---
+++ mul_float.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class mul_float{
+    public static void main(String[] args){
+        float a=1.4f;
+        float b=2.3f;
+        float c=a*b;
+        System.out.println("multiplication of given number is  "+c);
+    }
+}
