39e9a9cd90b0687bf292e20f9baab24ef44db36c
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-02-05 11:14:51 +0530

    swaping of two numbers

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap{
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        int tem=a;
+        a=b;
+        b=tem;
+        System.out.println("After swaping a is "+a);
+        System.out.println("After swaping b is "+b);
+    }
+}
