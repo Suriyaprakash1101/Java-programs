5d0006d0bd1acd870c6f16eccef816d8862b9b63
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2023-02-14 20:28:17 +0530

    pattern programme for triangle using method

---
+++ pa_tri.java
@@ -0,0 +1,12 @@
+public class pa_tri{
+    public static void main(String[] args){
+        pattern1(6);
+    }
+    static void pattern1(int n){
+        for(int row =1;row<n;row++){
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+}
